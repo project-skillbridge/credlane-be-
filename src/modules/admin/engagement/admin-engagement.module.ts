e8b7bc9d5e1950a157c5e23597734ac0234d13af
oduwoleeyinojuoluwa44 <oduwoleeyinojuoluwa44@gmail.com>
2026-07-04 11:30:41 +0100

    feat(admin-engagement): add AdminEngagementController and module
    GET /admin/engagement/stats
    GET /admin/engagement/retake-dropoff
    GET /admin/engagement/minor-uptake
    
    All gated @AdminTiers(SUPER_ADMIN, ADMIN, REVIEWER).

---
+++ src/modules/admin/engagement/admin-engagement.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AssessmentAttempt } from '../../assessments/entities/assessment-attempt.entity';
+import { AdminEngagementController } from './admin-engagement.controller';
+import { AdminEngagementService } from './admin-engagement.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([AssessmentAttempt])],
+  controllers: [AdminEngagementController],
+  providers: [AdminEngagementService],
+})
+export class AdminEngagementModule {}
